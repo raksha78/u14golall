28bc4727324ded38ae6f73a2dad8b4b978110847
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-13 14:16:33 +0530

    updating go

--- test/_go.sh
+++ test/_go.sh
@@ -3,7 +3,7 @@
 #set the source path of gvm. $HOME points to '/root'
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm
 
-declare -a versions=( '1.1' '1.1.2' '1.2' '1.2.2' '1.3' '1.3.3' '1.4' '1.4.3' '1.5' '1.5.4' '1.6' '1.6.4' '1.7' '1.7.5' )
+declare -a versions=( '1.7.6' '1.8.3' )
 for version in "${versions[@]}"
   do
     echo "######### Testing Go$version ###################"
