df8b28a9013f7295fb61bb72502063a9bbea32d6
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-13 16:35:56 +0530

    update go

--- README.md
+++ README.md
@@ -3,14 +3,9 @@ u14golall
 
 Shippable CI image for Go on Ubuntu 14.04 with all services available. Available versions are:
 
-1. 1.1
-2. 1.2
-3. 1.3
-4. 1.4
-5. 1.5
-6. 1.6
-7. 1.7
-8. 1.7.5
+1. 1.7.6
+2. 1.8.3
+
 
 **Services:**
 use this link https://github.com/dry-dock/u14all to refer to the versions of the services
--- go_install.sh
+++ go_install.sh
@@ -5,14 +5,12 @@
 export  CGO_ENABLED=0
 
 
- gvm install go1.4 && gvm use go1.4 && go install -a std && go get -u github.com/tools/godep
-
-
+ 
 declare -a versions=( '1.7.6' '1.8.3' )
 for version in "${versions[@]}"
   do
      echo "================= Install Go $version==================="
-     gvm use go1.4
+     
      export GOROOT_BOOTSTRAP=$GOROOT
      gvm install go$version && gvm use go$version && go install -a std && go get -u github.com/tools/godep
 done
